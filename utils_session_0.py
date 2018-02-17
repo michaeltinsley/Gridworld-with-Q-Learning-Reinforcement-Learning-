4e0219e1fbee7f02fb66e39ced975553bb64db1e
Michael Tinsley <michaeltinsley@outlook.com>
2018-02-17 12:01:59 +0000

    Tidy up

--- utils_session_0.py
+++
@@ -1,5 +0,0 @@
-# This file is imported from within the RL-lab-0.ipynb Jupyter notebook.
-# You can write code here that you do not want to show in your notebook.
-
-def utils_test():
-    return "This is the output of the utils_test() function that was successfully imported from the utils_session_0.py file."
